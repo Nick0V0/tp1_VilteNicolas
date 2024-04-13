ea7f1b7529cc68fb21ea2aece3cd24e911fb72b7
Vilte <nicolasvilte08@gmail.com>
2024-04-13 16:33:32 -0300

    Inicio del proyecto TPO1

---
+++ src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,21 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String NomPais = "Argentina";
+		byte Edad = 76;
+		double AlturaEdi = 150.5;
+		double PrecioProd = 1999.99;
+		String NumTelf = "+54 11 12345678";
+		double Coseno = Math.cos(0.5);
+		System.out.println("Nombre del país: " + NomPais);
+		System.out.println("La edad de una persona: " + Edad + "Metros");
+		System.out.println("La altura de un edificio: " + AlturaEdi);
+		System.out.println("El precio de un producto de supermercado: " + PrecioProd + " $");
+		System.out.println("Número de teléfono: " + NumTelf);
+		System.out.println("El cálculo de coseno de 0.5: " + Coseno);
+	}
+
+}
+		
